c69ba929a73dbfb36a4f26ac229f170b2579b7c0
Cloud2602 <s332252@studenti.polito,it>
2024-08-08 19:11:54 +0200

    implemented tlb

--- os161-base-2.0.3/kern/include/vm_tlb.h
+++ os161-base-2.0.3/kern/include/vm_tlb.h
@@ -1,20 +1,60 @@
 #ifndef _VM_TLB_H_
 #define _VM_TLB_H_
 
+#include "types.h"
+#include "syscall.h"
+#include "proc.h"
+#include "opt-debug.h"
+
+
 /*
 * struttura dati per la gestione della TLB
 */
 struct tlb{
-    /* data */
+  
 };
 
+
 /*
-* funzione per rimuovere una entry dalla tlb 
-* in caso di miss. 
-* // TODO: CLAPE
-* @return  indice entry rimossa, 
-* -1 in caso di errore
+*Questa è una funzione che trova la vittima
+* @return indice della vittima
 */
-int tlb_remove(void);
+int tlb_victim(void);
+
+//riguardare non sono sicura
+/* 
+*Questa funzione mi dice se l'indirizzo è nel text segment e qualora non sia scrivibile
+* @param indirizzo virtuale
+* @return 1 se è nel text segment e non scrivibile, 0 altrimenti
+*/
+int segment_is_readonly(vaddr_t vaddr);
+
+/*
+* Questa funzione inserisce l'indirizzo del fault nella TLB con il corrispondente indirizzo fisico
+*@param indirizzo virtuale
+*@param indirizzo fisico
+
+*/
+int tlb_insert(vaddr_t vaddr, paddr_t paddr);
+
+/*
+* Questa funzione mi dice se laentry della tlb è valids 
+* il valid bit 
+*/
+int tlb_entry_is_valid(int index);
+
+/*
+* Questa funzione invalida le entry corrispondenti all'indirizzo fisico 
+* Utile quando una entry nella pt è invalidata e bisogna invalidare anche la corrispondente entry nella tlb
+* @param indirizzo fisico
+* @return 0 in caso di successo, -1 in caso di errore
+*/
+ int tlb_invalidate_entry(paddr_t paddr);
+
+ /* 
+ *Questa funzione è usata per invalidare la TLB quando c'è uno switch da un processo all'altro
+ * La TLB è comune per tutti i processi e non ha un campo "pid"
+ */
+void tlb_invalidate_all(void);
 
 #endif /* _VM_TLB_H_ */
--- os161-base-2.0.3/kern/vm/vm_tlb.c
+++ os161-base-2.0.3/kern/vm/vm_tlb.c
@@ -1,17 +1,54 @@
 // codice per manipolare la tlb (incluso replacement)
 #include "vm_tlb.h"
-
 #include "vm.h"
 #include "vmstats.h"
-
-//todo: implementare le funzioni di manipolazione della tlb
-int tlb_remove(void){
-    return -1;
-}
+#include "mips/tlb.h"
+#include "addrspace.h"
+#include "spl.h"
+#include "pt.h"
 
 /*
 * usata per gestire il tlb miss
 */
 int vm_fault(int faulttype, vaddr_t faultaddress){
     return -1;
+}
+
+int tlb_victim(void){
+    // RR algoritmo
+    int victim;
+    static unsigned int next_victim = 0;
+    victim = next_victim;
+    next_victim = (next_victim + 1) % NUM_TLB;
+    return victim;
+}
+
+int segment_is_readonly(vaddr_t vaddr){
+    struct addrspace *as;
+    as = proc_getas();
+    int readonly = 0;
+    unit32_t vbfirst_text_vaddr = as->as_vbase1;
+    inti size = as-> as_npages1;
+    unit32_t last_text_vaddr = (size * PAGE_SIZE) + first_text_vaddr;
+    if(vaddr >= vbase && vaddr < last_text_vaddr){
+        readonly = 1;
+    }
+    return readonly;
+}
+
+int tlb_insert(vaddr_t faultvaddr, paddr_t faultpaddr){
+    
+    int i, valid, readonly;
+    uint32_t ehi, elo;
+    readonly = segment_is_readonly(faultvaddr);
+
+    //cerco una entry libera e aggiorno statistiche 
+    for(i = 0; i < NUM_TLB; i++){
+        if(!tlb_entry_is_valid(i)){
+            stats_tlb_fault(FREE_FAULT);
+            break;
+        }
+    }
+      
+    return 0;
 }
