bcbcfe2a1a6ff9bbedb498f87cc2a5cd90c05d1d
Cloud2602 <s332252@studenti.polito,it>
2024-08-13 21:21:17 +0200

    tlb insert

--- os161-base-2.0.3/kern/vm/vm_tlb.c
+++ os161-base-2.0.3/kern/vm/vm_tlb.c
@@ -36,19 +36,71 @@ int segment_is_readonly(vaddr_t vaddr){
     return readonly;
 }
 
-int tlb_insert(vaddr_t faultvaddr, paddr_t faultpaddr){
-    
-    int i, valid, readonly;
-    uint32_t ehi, elo;
-    readonly = segment_is_readonly(faultvaddr);
 
-    //cerco una entry libera e aggiorno statistiche 
-    for(i = 0; i < NUM_TLB; i++){
-        if(!tlb_entry_is_valid(i)){
-            stats_tlb_fault(FREE_FAULT);
-            break;
+int tlb_insert(vaddr_t faultvaddr, paddr_t faultpaddr){
+    /* faultpaddr è l'indirizzo dell'inizio del frame fisico, quindi devo ricordare che non devo passare l'intero indirizzo ma devo mascherare gli ultimi 12 bit */
+    int entry, valid, readonly; 
+    uint32_t hi, lo, prevHi, prevLo;
+    readonly = segment_is_readonly(faultvaddr); 
+
+    /*step 1: cerca una entry libera e aggiorna la statistica corrispondente (FREE)*/
+    for(entry = 0; entry < NUM_TLB; entry++){
+        valid = tlb_entry_is_valid(entry);
+        if(!valid){
+            
+                hi = faultvaddr;
+                lo = faultpaddr | TLBLO_VALID;
+                /*is the segment a text segment?*/
+               if( readonly){
+                    /*Devo impostare un bit di dirty che essenzialmente è un privilegio di scrittura*/
+                    lo = lo | TLBLO_DIRTY; 
+                }
+                tlb_write(hi, lo, entry);
+            /*aggiotno tlb fault free*/
+            add_tlb_type_fault(FREE_FAULT); //TO DO: implementare add_tlb_type_fault, capire esattamente quale fault
+            
+            return 0;
         }
+
     }
-      
+    /*step 2: Non ho trovato una entry invalida, quindi,,, cerco una vittima, sovrascrivo, aggiorno la statistica corrispondente */
+    entry = tlb_victim();
+    hi = faultvaddr;
+    lo = faultpaddr | TLBLO_VALID;
+    /
+    if( readonly){
+        /*Devo impostare un bit di dirty che essenzialmente è un privilegio di scrittura*/
+        lo = lo | TLBLO_DIRTY; 
+    }
+    
+    tlb_read(&prevHi, &prevLo, entry);
+    //TODO: notificare alla pt che quella entry non c'è piu nella tlb da capire update_tlb_bit(prevHi, curproc->p_pid);
+    
+    tlb_write(hi, lo, entry);
+    /*update tlb faults replace*/
+    add_tlb_type_fault(FREE_FAULT); //TO DO: implementare add_tlb_type_fault, capire esattamente quale fault
+    return 0;
+
+}
+
+int tlb_entry_is_valid(int i){
+    uint32_t hi, lo;
+    tlb_read(&hi, &lo, i);
+    /* quindi estrai il bit di validità e restituisci il risultato. Se il risultato è 0 significa che il bit di validità non è impostato e quindi l'entry è invalida. */
+    return (lo & TLBLO_VALID);
+}
+
+int tlb_invalidate_entry(paddr_t paddr){
+    //trovo il match nella tlb
+    paddr_t frame_addr_stored;
+    paddr_t frame_addr = paddr & TLBLO_PPAGE; 
+    uint32_t hi, lo;
+    for(int i = 0; i<NUM_TLB; i++){
+        tlb_read(&hi, &lo, i);
+        frame_addr_stored = lo & TLBLO_PPAGE ; // estraggo il phisical address dallo store
+        if(frame_addr_stored == frame_addr)
+            tlb_write(TLBHI_INVALID(i), TLBLO_INVALID());
+    }
     return 0;
 }
+
