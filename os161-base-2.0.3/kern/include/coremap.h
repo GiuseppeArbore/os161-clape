15da4cd98ab4508dcf3d89e19c30a6fc022ae384
GiuseppeArbore <s329535@studenti.polito.it>
2024-08-14 12:59:32 +0200

    coremap done

--- os161-base-2.0.3/kern/include/coremap.h
+++ os161-base-2.0.3/kern/include/coremap.h
@@ -1,36 +1,40 @@
 #ifndef _VM_COREMAP_H_
 #define _VM_COREMAP_H_
 
+#include <types.h>
+
+/*
+* bitmap che tiene traccia dei frame fisici liberi
+*/
 int *bitmapFreeFrames; // bitmap tieni traccia dei frame liberi
-int bitmapFreeFramesActive = 0; // flag per sapere se bitmap è attivo
 
 
 /*
-* Questa è usata per ottenere un frame libero   
+* funzione usata per ottenere un frame libero   
 *
 * @return l'indice del frame libero
 */
 int get_frame(void);
 
 /*
-* Questa è usata per liberare un frame
+* funzione usata per liberare un frame
 *
 * @param frame l'indice del frame da liberare
 */
 void free_frame(int);
 
 /*
-* Questa è usata per inizializzare la bitmap
+* Qfunzione usata per inizializzare la bitmap
 */
 void bitmap_init(void);
 
 /*
-* Questa è usata per distruggere la bitmap
+* funzione usata per distruggere la bitmap
 */
 void bitmap_destroy(void);
 
 /*
-* Questa è usata per sapere se la bitmap è attiva
+* funzione usata per sapere se la bitmap è attiva
 */
 int bitmap_is_active(void);
 
