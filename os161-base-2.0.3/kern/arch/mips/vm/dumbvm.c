842b6c8bbc7c328f9eaa3136aa6f7e66df0365ea
GiuseppeArbore <s329535@studenti.polito.it>
2024-09-13 11:19:59 +0200

    eliminato cose dei lab

--- os161-base-2.0.3/kern/arch/mips/vm/dumbvm.c
+++ os161-base-2.0.3/kern/arch/mips/vm/dumbvm.c
@@ -76,8 +76,6 @@
 
 static struct spinlock freemem_lock = SPINLOCK_INITIALIZER;
 
-
-
 void
 vm_bootstrap(void)
 {
@@ -105,84 +103,22 @@ dumbvm_can_sleep(void)
 	}
 }
 
-static paddr_t 
-getfreeppages(unsigned long npages) {
-  paddr_t addr;	
-  long i, first, found, np = (long)npages;
 
-  if (!isTableActive()) return 0; 
-  spinlock_acquire(&freemem_lock);
-  for (i=0,first=found=-1; i<nRamFrames; i++) {
-    if (freeRamFrames[i]) {
-      if (i==0 || !freeRamFrames[i-1]) 
-        first = i; /* set first free in an interval */   
-      if (i-first+1 >= np) {
-        found = first;
-        break;
-      }
-    }
-  }
-	
-  if (found>=0) {
-    for (i=found; i<found+np; i++) {
-      freeRamFrames[i] = (unsigned char)0;
-    }
-    allocSize[found] = np;
-    addr = (paddr_t) found*PAGE_SIZE;
-  }
-  else {
-    addr = 0;
-  }
-
-  spinlock_release(&freemem_lock);
-
-  return addr;
-}
-
-//TODO: CLAPE: capire se mettere come prima dei lab
-/*
 static paddr_t
 getppages(unsigned long npages)
 {
-  paddr_t addr;
+	paddr_t addr;
 
-  // try freed pages first //
-  addr = getfreeppages(npages);
-  if (addr == 0) {
-    // call stealmem //
-    spinlock_acquire(&stealmem_lock);
-    addr = ram_stealmem(npages);
-    spinlock_release(&stealmem_lock);
-  }
-  if (addr!=0 && isTableActive()) {
-    spinlock_acquire(&freemem_lock);
-    allocSize[addr/PAGE_SIZE] = npages;
-    spinlock_release(&freemem_lock);
-  } 
 
-  return addr;
+	spinlock_acquire(&stealmem_lock);
+	addr = ram_stealmem(npages);
+	spinlock_release(&stealmem_lock);
+  
+
+	return addr;
 }
 
 
-
-static int 
-freeppages(paddr_t addr, unsigned long npages){
-  long i, first, np=(long)npages;	
-
-  if (!isTableActive()) return 0; 
-  first = addr/PAGE_SIZE;
-  KASSERT(allocSize!=NULL);
-  KASSERT(nRamFrames>first);
-
-  spinlock_acquire(&freemem_lock);
-  for (i=first; i<first+np; i++) {
-    freeRamFrames[i] = (unsigned char)1;
-  }
-  spinlock_release(&freemem_lock);
-
-  return 1;
-}
-*/
 
 /* Allocate/free some kernel-space virtual pages */
 vaddr_t
@@ -198,16 +134,11 @@ alloc_kpages(unsigned npages)
 	return PADDR_TO_KVADDR(pa);
 }
 
-//TODO: cambiare 
+
 void 
 free_kpages(vaddr_t addr){
-  if (isTableActive()) {
-    paddr_t paddr = addr - MIPS_KSEG0;
-    long first = paddr/PAGE_SIZE;	
-    KASSERT(allocSize!=NULL);
-    KASSERT(nRamFrames>first);
-    freeppages(paddr, allocSize[first]);	
-  }
+
+  (void) addr;
 }
 
 void
